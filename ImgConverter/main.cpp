94563f196cc05a8245a4160e83b5a62fc263d55d
Daria Chepurnaia <d89299926427@yandex.ru>
2024-04-19 01:52:12 +0300

    fist commit

---
+++ ImgConverter/main.cpp
@@ -0,0 +1,117 @@
+#include <img_lib.h>
+#include <jpeg_image.h>
+#include <ppm_image.h>
+#include <bmp_image.h>
+
+#include <filesystem>
+#include <string_view>
+#include <iostream>
+
+using namespace std;
+
+enum class FORMAT {
+    JPEG,
+    PPM, 
+    BMP,
+    UNKNOWN
+};
+
+class ImageFormatInterface {
+public:
+    virtual bool SaveImage(const img_lib::Path& file, const img_lib::Image& image) const = 0;
+    virtual img_lib::Image LoadImage(const img_lib::Path& file) const = 0;
+};
+
+class BMPInterface: public ImageFormatInterface {
+    bool SaveImage(const img_lib::Path& file, const img_lib::Image& image) const override {
+        return img_lib::SaveBMP(file, image);        
+    }
+    img_lib::Image LoadImage(const img_lib::Path& file) const override {
+        return img_lib::LoadBMP(file);
+    }
+};
+
+class PPMInterface: public ImageFormatInterface {
+    bool SaveImage(const img_lib::Path& file, const img_lib::Image& image) const override {
+        return SavePPM(file, image);        
+    }
+    img_lib::Image LoadImage(const img_lib::Path& file) const override {
+        return img_lib::LoadPPM(file);
+    }
+};
+
+class JPEGInterface: public ImageFormatInterface {
+    bool SaveImage(const img_lib::Path& file, const img_lib::Image& image) const override {
+        return SaveJPEG(file, image);        
+    }
+    img_lib::Image LoadImage(const img_lib::Path& file) const override {
+        return img_lib::LoadJPEG(file);
+    }
+};
+
+static const PPMInterface ppmInterface;
+static const JPEGInterface jpegInterface;
+static const BMPInterface bmpInterface;
+
+FORMAT GetFormatByExtension(const img_lib::Path& input_file) {
+    const string ext = input_file.extension().string();
+    if (ext == ".jpg"sv || ext == ".jpeg"sv) {
+        return FORMAT::JPEG;
+    }
+
+    if (ext == ".ppm"sv) {
+        return FORMAT::PPM;
+    }
+    
+    if (ext == ".bmp"sv) {
+        return FORMAT::BMP;
+    }
+
+    return FORMAT::UNKNOWN;
+}
+
+ const ImageFormatInterface* GetFormatInterface(const img_lib::Path& path) {
+    switch(GetFormatByExtension(path)) {
+        case FORMAT::PPM: return &ppmInterface;
+        case FORMAT::JPEG: return &jpegInterface;
+        case FORMAT::BMP: return &bmpInterface;
+        default: return nullptr;
+    }
+}
+
+int main(int argc, const char** argv) {
+    if (argc != 3) {
+        cerr << "Usage: "sv << argv[0] << " <in_file> <out_file>"sv << endl;
+        return 1;
+    }
+
+    img_lib::Path in_path = argv[1];
+    img_lib::Path out_path = argv[2];
+    
+    auto in_interface = GetFormatInterface(in_path);
+    if (!in_interface) {
+        cerr << "Unknown format of the input file"sv << endl;        
+        return 2;
+    }
+    auto out_interface = GetFormatInterface(out_path);
+     if (!in_interface) {
+        cerr << "Unknown format of the output file"sv << endl;        
+        return 3;
+    }
+    
+    
+    //img_lib::Image image = img_lib::LoadPPM(in_path);
+    img_lib::Image image = in_interface->LoadImage(in_path);
+    if (!image) {
+        cerr << "Loading failed"sv << endl;
+        return 4;
+    }
+
+    //if (!img_lib::SaveJPEG(out_path, image)) {
+    if (!out_interface->SaveImage(out_path, image)) {
+        cerr << "Saving failed"sv << endl;
+        return 5;
+    }
+
+    cout << "Successfully converted"sv << endl;
+}
---
+++ ImgLib/bmp_image.cpp
@@ -0,0 +1,105 @@
+#include "bmp_image.h"
+#include "pack_defines.h"
+
+#include <array>
+#include <fstream>
+#include <string_view>
+
+using namespace std;
+
+namespace img_lib {
+
+PACKED_STRUCT_BEGIN BitmapFileHeader {   
+    char symbols[2] = {'B', 'M'};
+    uint32_t size;
+    uint32_t space{};
+    uint32_t indent_data = 54;
+}
+PACKED_STRUCT_END
+
+PACKED_STRUCT_BEGIN BitmapInfoHeader {    
+    uint32_t size_of_header = 40; 
+    int32_t width; 
+    int32_t height;
+    uint8_t color_planes_num = 1;
+    uint8_t bits_per_pixel = 24;
+    uint32_t compression_type = 0;
+    uint32_t num_bytes_of_data; //Произведение отступа на высоту
+    int32_t horDPI = 11811; //Горизонтальное разрешение
+    int32_t verDPI = 11811; //Вертикальное разрешение
+    int32_t num_of_colors_used = 0; //Количество использованных цветов 
+    int32_t num_of_colors = 0x1000000; //Количество значимых цветов  
+}
+PACKED_STRUCT_END
+
+// функция вычисления отступа по ширине
+static int GetBMPStride(int w) {
+    return 4 * ((w * 3 + 3) / 4);
+}
+
+bool SaveBMP(const Path& file, const Image& image) {
+    ofstream out(file, ios::binary);
+    BitmapFileHeader bf;
+    BitmapInfoHeader bi;
+    
+    int stride = GetBMPStride(image.GetWidth());
+    
+    bf.size = stride * image.GetHeight() + 54;
+    bi.width = image.GetWidth(); 
+    bi.height = image.GetHeight();
+    bi.num_bytes_of_data = stride * bi.height;
+    
+    out.write((char*)&bf, sizeof(bf));
+    out.write((char*)&bi, sizeof(bi));
+    
+    const int w = image.GetWidth();
+    const int h = image.GetHeight();
+    std::vector<char> buff(stride);
+
+    for (int y = h-1; y > 0; --y) {
+        const Color* line = image.GetLine(y);
+        for (int x = 0; x < w; ++x) {
+            buff[x * 3 + 0] = static_cast<char>(line[x].b);
+            buff[x * 3 + 1] = static_cast<char>(line[x].g);
+            buff[x * 3 + 2] = static_cast<char>(line[x].r);
+        }
+        out.write(buff.data(), stride);
+    }
+    return out.good();    
+}
+ 
+Image LoadBMP(const Path& file) {
+    ifstream ifs(file, ios::binary);
+    BitmapFileHeader bf;
+
+    ifs.read((char*)&bf, sizeof(bf));
+    
+    if (!ifs || bf.symbols[0] != 'B' || bf.symbols[1] != 'M' || bf.space != 0 || bf.indent_data != 54) {
+        return {};
+    }
+   
+    BitmapInfoHeader bi;
+    
+    ifs.read((char*)&bi, sizeof(bi));    
+    if (!ifs) return {};
+    
+    int stride = GetBMPStride(bi.width);
+    if (bf.size != stride * bi.height + 54) return {};
+    
+    Image result(bi.width, bi.height, Color::Black());
+    std::vector<char> buff(stride);
+    
+    for (int y = bi.height-1; y > 0; --y) {
+        Color* line = result.GetLine(y);
+        ifs.read(buff.data(), stride);
+
+        for (int x = 0; x < bi.width; ++x) {
+            line[x].r = static_cast<byte>(buff[x * 3 + 2]);
+            line[x].g = static_cast<byte>(buff[x * 3 + 1]);
+            line[x].b = static_cast<byte>(buff[x * 3 + 0]);
+        }
+    }
+    return result;
+}
+
+}  // namespace img_lib
